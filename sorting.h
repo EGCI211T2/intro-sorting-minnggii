aebf03d1056251210aa981715618ebf0e52f20f3
minnggii <54227292+minnggii@users.noreply.github.com>
2026-01-28 07:43:02 +0000

    assignment 4

--- sorting.h
+++ sorting.h
@@ -12,7 +12,7 @@ void display(int a[],int N)
 {
     for(int i = 0; i < N; i++)
     {
-        cout<<setw(3)<<a[i];
+        cout<<setw(4)<<a[i];
         
     }
     cout<<endl;
@@ -23,15 +23,15 @@ void sort(int *a,int N)
     int i, j;
     for(j = 0; j < N-1; j++)
     {
-        for(i = 0; i < N - 1; i++)
+        for(i = 0; i < N-1-j; i++)
             {
             // For ascending order
             if(a[i] > a[i + 1])
             {
                 swap(a[i], a[i + 1]);
             }
-            //display(a, N);
         }
+        display(a, N);
     }
 }
 
